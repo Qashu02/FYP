44b1ce7c564b506fa04639850337db2f97b8a026
Qashu02 <hafizqasim5566@gmail.com>
2025-04-13 12:01:51 -0700

    Booking History

--- MyNewProject/App.js
+++ MyNewProject/App.js
@@ -1,19 +1,18 @@
 // App.js
 import React from 'react';
 
-import MenuDetailsScreen from './Screens/MenuDetailsScreen';
+
 
 import { View,StyleSheet } from 'react-native';
-import ReviewSection from './components/Blog/ReviewSection';
-import MessageScreen from './Screens/MessageScreen';
-import ProfileScreen from './Screens/ProfileScreen';
 
+import BookingHistory from './Screens/BookingHistoryScreen';
+import BookingHistoryScreen from './Screens/BookingHistoryScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-<ProfileScreen/>
+<BookingHistoryScreen/>
     </View>
    
    
